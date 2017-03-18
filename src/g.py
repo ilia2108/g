6857af9215549113aa15415a898eaf628fdf8a43
ilia2108 <ilya210819993@gmail.com>
2017-03-19 00:30:48 +0300

    edit time

--- src/g.py
+++ src/g.py
@@ -1,4 +1,6 @@
 import nltk.data
+import time
+st = time.time()
 fs = open('input', 'r', encoding='utf-8').read().split('\n')
 is_text = fs[1]
 tokenizer = nltk.data.load('russian.pickle')
@@ -7,5 +9,5 @@ cnt = 0
 for s in sentences:
     if fs[0].upper() in s: cnt += 1
 print (cnt)
-
+print((time.time() - st) % 60)
 
